779cc6807d65635920546058307c0c397988dbdd
kollarmark11 <kollarmark11@gmail.com>
2020-07-18 14:45:03 +0200

    jul-18

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,54 +1,62 @@
 <template>
   <div class="container-fluid">
     <v-row justify="center">
-      <form name="contact" method="POST" data-netlify="true">
-        <v-form ref="form" v-model="valid" lazy-validation>
-          <v-col md="10">
-            <v-text-field
-              v-model="lastname"
-              label="Vezetéknév"
-              :rules="nameRules"
-              r
-              equired
-              width="100"
-            ></v-text-field>
+      <v-card width="600" class="my-15">
+        <v-card-text>
+          <form name="contact" method="POST" data-netlify="true">
+            <v-col md="12" class="ma-0 py-3">
+              <input type="text" name="name" placeholder="Név" />
 
-            <v-text-field
-              v-model="lastname"
-              label="Keresztnév"
-              :rules="nameRules"
-              r
-              equired
-            ></v-text-field>
+              <input type="text" name="name" placeholder="E-mail" />
+              <textarea
+                name="message"
+                cols="30"
+                rows="5"
+                placeholder="Üzenet"
+              ></textarea>
 
-            <v-text-field
-              v-model="lastname"
-              label="Email"
-              :rules="nameRules"
-              r
-              equired
-            ></v-text-field>
-          </v-col>
-          <v-btn type="submit">Küldés</v-btn>
-        </v-form>
-      </form>
+              <div class="button">
+                <button type="submit">
+                  Küldés
+                </button>
+              </div>
+            </v-col>
+          </form>
+        </v-card-text>
+      </v-card>
     </v-row>
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      nameRules: [
-        v => !!v || "Kötelező mező!",
-        v => (v && v.length <= 30) || "Name must be less than 10 characters"
-      ],
-      emailRules: [
-        v => !!v || "Kötelező mező!",
-        v => /.+@.+\..+/.test(v) || "E-mail must be valid"
-      ]
-    };
-  }
-};
+export default {};
 </script>
+
+<style lang="scss" scoped>
+.container-fluid {
+  background-color: #6a1b9a;
+}
+
+input,
+textarea {
+  background-color: #e0e0e0;
+  padding: 10px;
+  margin: 20px;
+  width: 500px;
+  border-radius: 10px;
+}
+
+.label {
+  margin-bottom: 20px;
+}
+.button {
+  margin: 0 auto;
+  text-align: center;
+  button {
+    background-color: #6a1b9a;
+    padding: 10px 20px;
+    color: #ffffff;
+    border-radius: 10px;
+  }
+}
+</style>
